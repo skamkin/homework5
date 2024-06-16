6bb589294e665c71ccaf4e4d93d38db1b845cb2f
skamkin <doc.kamkin@gmail.com>
2024-06-16 09:46:18 +0500

    Add files via upload

---
+++ homework5.1.py
@@ -0,0 +1,14 @@
+immutable_var = 1,2,3, False, 'First'
+print(immutable_var)
+tuple_1 =([1,2], 3, True)
+print(tuple_1)
+tuple_1[0][0] = 5
+print(tuple_1)
+tuple_2 = (1,2,['Elena','Katy'])
+print(tuple_2)
+tuple_2[2][1] = 'Irina'
+print(tuple_2)
+mutable_list = [4,5,'Elena','Katy']
+print(mutable_list)
+mutable_list[2] = 'Ivan'
+print(mutable_list)
